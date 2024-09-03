cae158104e82348d39d7fc8bd43506dcee6eb5d0
Herbert Wirnsberger <herbert.wirnsberger@gepardec.com>
2024-09-03 16:39:31 +0200

    keine ahnung was ich da mache...

--- renovate-config.js
+++ renovate-config.js
@@ -19,6 +19,7 @@ module.exports = {
     recreateWhen: "always",
     packageRules: [
         {
+            groupName: "{{depNameSanitized}}",
             groupSlug: "{{depNameSanitized}}#{{currentVersion}}#{{newVersion}}"
         }
     ],
