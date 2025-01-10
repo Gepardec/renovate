b80f3d7cb8bca8ea1345ed05dc7b9a6e00d4c582
Ollitod <olivertod11@yahoo.de>
2025-01-10 10:36:17 +0100

    Configure renovate for angular project

--- renovate-config.js
+++ renovate-config.js
@@ -2,16 +2,15 @@ module.exports = {
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
     platform: 'github',
     repositories: [
-        "Gepardec/jboss-examples"
-        // "Gepardec/renovate-playground"
+        "Gepardec/renovate-angular-playground"
     ],
     prHourlyLimit: 0,
     separateMajorMinor: false,
     packageRules: [
         {
-            matchPackagePrefixes: ["io.quarkus"],
-            groupName: "quarkus",
-            groupSlug: "quarkus_{{currentVersion}}_{{newVersion}}"
+            matchPackagePrefixes: ["@angular/**"],
+            groupName: "angular",
+            groupSlug: "angular_{{currentVersion}}_{{newVersion}}"
         },
     ],
     recreateWhen: "always"
