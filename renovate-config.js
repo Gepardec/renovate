1effa7dd0ac8dcd760cc9c373d8730ab4d8ca7dc
Tod Oliver <olivertod11@yahoo.de>
2025-01-10 11:46:25 +0100

    remove project-specific config

--- renovate-config.js
+++ renovate-config.js
@@ -5,14 +5,5 @@ module.exports = {
         "Gepardec/renovate-angular-playground"
     ],
     prHourlyLimit: 0,
-    separateMajorMinor: false,
-    packageRules: [
-        {
-            matchDatasources: ["npm"],
-            matchPackageNames: ["@angular/**"],
-            groupName: "Angular",
-            groupSlug: "angular_{{currentVersion}}_{{newVersion}}"
-        },
-    ],
     recreateWhen: "always"
 }
