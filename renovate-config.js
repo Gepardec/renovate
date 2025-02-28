5da08f75928d8a62524fdb7598d0c29430b0762e
Christoph Ruhsam <ruhsamchristoph@gmail.com>
2025-02-28 08:26:57 +0100

    Update renovate-config.js

--- renovate-config.js
+++ renovate-config.js
@@ -2,7 +2,8 @@ module.exports = {
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
     platform: 'github',
     repositories: [
-        "Gepardec/jboss-examples"
+        "mega-backend"
+        // "Gepardec/jboss-examples"
         // "Gepardec/renovate-playground"
     ],
     prHourlyLimit: 0,
